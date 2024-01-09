99b17da245e16453aefd1141abc382c9e461250c
Raaz <21ketanraaz@gmail.com>
2024-01-09 19:34:05 +0530

    Instead of storing the data in the new state ue store it in the state which is prviously made setUserInput

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import Header from "./Components/Header/Header";
 import UserInput from "./Components/UserInput/UserInput";
 import ResultTable from "./Components/ResultTable/ResultTable";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  
+  const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
-    const yearlyData = []; // per-year results
-
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
+    setUserInput(userInput);
     // do something with yearlyData ...
+    //Now we want the updated dta to be displayed in the table
   };
+
+  const yearlyData = []; // per-year results
+
+  let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+  const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+  const expectedReturn = +userInput["expected-return"] / 100;
+  const duration = +userInput["duration"];
+
+  // The below code calculates yearly results (total savings, interest etc)
+  for (let i = 0; i < duration; i++) {
+    const yearlyInterest = currentSavings * expectedReturn;
+    currentSavings += yearlyInterest + yearlyContribution;
+    yearlyData.push({
+      // feel free to change the shape of the data pushed to the array!
+      year: i + 1,
+      yearlyInterest: yearlyInterest,
+      savingsEndOfYear: currentSavings,
+      yearlyContribution: yearlyContribution,
+    });
+  }
   return (
     <div>
       <Header />
